757404e0c540069e35ac9e71bbb3804e99c7d27f
dukenyabuti <99273249+dukenyabuti@users.noreply.github.com>
2023-08-31 20:22:01 +0300

    flask

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
+#"this function imports Flask to my code"
 app = Flask(__name__)
-
+#this functions directs the program where to get some templets 
 @app.route("/")
 def HBNB():
     return "<p>HELLO HBNB</p>"
